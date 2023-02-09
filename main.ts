83cddec2d4bd2477798a873ddab6fa460be2c97e
JackyJia12345 <124847810+JackyJia12345@users.noreply.github.com>
2023-02-09 09:15:58 +0800

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,11 @@
+basic.forever(function () {
+    if (input.pinIsPressed(TouchPin.P0)) {
+        basic.showLeds(`
+            # . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            `)
+    }
+})
